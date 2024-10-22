e6b1fce452c9691775ea69fcba1c3c9af947fa0a
Arthur Felix Cruz <you@example.com>
2024-10-21 22:25:58 -0300

    adicionando botao de desligar som e mostrar informações do video

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 let soundButton = document.querySelector(".soundbutton")
+let soundOff = document.querySelector(".soundOff")
 let video = document.querySelector(".video")
+let infoButton = document.querySelector(".linkinfo")
+let modal = document.querySelector(".modal")
 
 soundButton.addEventListener("click", soundOn)
+soundOff.addEventListener("click", soundMuted)
 
 function soundOn(){
     video.muted = false
+}
+
+function soundMuted(){
+    video.muted = true
+}
+
+infoButton.addEventListener("click", showInfo)
+modal.addEventListener("click", hideInfo)
+
+function showInfo(){
+    modal.style.display = "block"
+}
+
+function hideInfo(){
+    modal.style.display = "none"
 }
